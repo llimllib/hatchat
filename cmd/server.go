d4df81ce8cb7167f570112a60b648c7384fe6d08
Bill Mill <bill@billmill.org>
2024-01-23 18:03:54 -0500

    serve on localhost by default
    this is how you avoid annoying mac popups

--- cmd/server.go
+++ cmd/server.go
@@ -6,7 +6,7 @@ import (
 	"github.com/llimllib/tinychat/server"
 )
 
-var addr = flag.String("addr", ":8080", "http service address")
+var addr = flag.String("addr", "localhost:8080", "tinychat address")
 
 func main() {
 	flag.Parse()
