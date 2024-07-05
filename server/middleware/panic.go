d34d63a0c18eef940ece6a9dbf695203912bf1b7
Bill Mill <bill@billmill.org>
2024-07-05 19:46:57 -0400

    chore: commit panic middleware
    and static directory

---
+++ server/middleware/panic.go
@@ -0,0 +1,25 @@
+package middleware
+
+import (
+	"log/slog"
+	"net/http"
+)
+
+// RecoverMiddleware is a middleware function that recovers from any panics and
+// returns a 500 Internal Server Error response.
+func RecoverMiddleware(logger *slog.Logger) func(http.HandlerFunc) http.HandlerFunc {
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				if err := recover(); err != nil {
+					logger.Error("Panic occurred", "err", err)
+
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("Internal Server Error"))
+				}
+			}()
+
+			next(w, r)
+		}
+	}
+}
